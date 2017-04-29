9d14fe277835441db5fe55ff871669a4143669b8
Caleb Bux <Caleb Bux>
2017-04-29 15:56:17 -0400

    most functionality is there, missing hangman animation and letters displaying

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,12 +1,21 @@
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-var guessedLetters = [];
+var guessedLettersArr = [];
+var pickedWord = "winner";
+var pickedWordChars = pickedWord.split("");
 var alphabetArea = document.getElementById("alphabet");
 var alphabetBtn = document.getElementsByClassName("alphabetBtn");
 var guessedTitle = document.getElementById("guessedTitle");
 var guessedLetters = document.getElementById("guessedLetters");
+var wordSpaces = document.getElementById("wordSpaces")
+
+
+for(i = 0; i < pickedWordChars.length; i++){
+	wordSpaces.innerHTML += '<div class="wordSpace"></div>';
+}
+
 
 for(i = 0; i < alphabet.length; i++){
-	alphabetArea.innerHTML = alphabetArea.innerHTML + '<button class="alphabetBtn"></button>';
+	alphabetArea.innerHTML += '<button class="alphabetBtn"></button>';
 
 }
 
@@ -14,16 +23,32 @@ for(i = 0; i < alphabet.length; i++){
 	alphabetBtn[i].textContent = alphabet[i];
 		alphabetBtn[i].addEventListener("click", function(){
 		this.classList.add("disabledBtn");
+		guessedLettersArr.push(this.textContent);
+		guessedLetter();
+		letterSelected(this.textContent);
 	}
 )}
 
-function letterSelected(letter) {
 
-	if (letter === ) {
-		//fill in that space in the word
+function guessedLetter(letter) {
+	guessedTitle.textContent = "Guessed Letters"
+	guessedLetters.innerHTML = " ";
+	for(i = 0; i < guessedLettersArr.length; i++)
+	guessedLetters.textContent += guessedLettersArr[i];
+}
+
+function letterSelected(letter) {
+	var result = 0;
+	for(i = 0; i < pickedWordChars.length; i++) {
+		
+		if (letter === pickedWordChars[i]) {
+			console.log(letter);
+		}
+		else {
+			result++;
+		}
 	}
-	else {
-		//add the letter to the letters selected area
-		guessedLetters.innerHTML = guessedLetters.innerHTML +  
+	if (result === pickedWordChars.length) {
+		console.log("Wrong")
 	}
 }
